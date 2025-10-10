5db5eead97e7188e0350a21e1556df6e93c5f5d2
Gray7915 <patrick.james.gray15@gmail.com>
2025-10-11 10:40:57 +1300

    to make git happy

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,10 +55,4 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-jlink {
-    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
-    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-    launcher {
-        name = "app"
-    }
-}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module com.prepper {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires java.sql;
     requires org.jdbi.v3.core;
     requires org.jdbi.v3.sqlobject;
     requires java.desktop;
+    requires org.slf4j;
 
     opens com.prepper to javafx.fxml;
+    opens com.domain to org.jdbi.v3.core; // <-- add this line
     exports com.prepper;
 }
